24492e96bdc0a44d1d3ad1e805c9a59e0542a10e
Bobby Shunk <Bobby@MacBook-Pro-5.local>
2017-05-25 14:06:16 -0500

    updated and finished 1st For-Loop

--- for-loops.js
+++ for-loops.js
@@ -1,5 +1,7 @@
 //Write a for loop that uses a counter variable initialized at 5 and then increments it by 10 every time it executes. Use console.log() to output the value of the counter variable each time through the loop. Stop execution of the loop if the counter variable's value is greater than 120.
 
-for (var i = 5; i < 120; i += 10);
-	console.log("Current value is ${i}")
+for (var i = 5; i < 120; i = i + 10) {
+	console.log(`Current value is ${i}`)
 }
+
+console.log("It works.")
