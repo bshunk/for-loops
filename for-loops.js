a2e2004014115128ef4be35f106aacdf7cbfce1b
Bobby Shunk <Bobby@MacBook-Pro-5.local>
2017-05-25 13:22:20 -0500

    updated loop JS

--- for-loops.js
+++ for-loops.js
@@ -1,3 +1,8 @@
-function countIncriment (incrimentNumber) {
+//Write a for loop that uses a counter variable initialized at 5 and then increments it by 10 every time it executes. Use console.log() to output the value of the counter variable each time through the loop. Stop execution of the loop if the counter variable's value is greater than 120.
 
+
+
+function count (incrimentNumber) {
+	for (var i = 5; i >= 5; i + 10) {
+	}
 };
